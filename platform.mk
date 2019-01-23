39b63150d44cd7927327d13b787478b13ac1342a
coderzstas <coderz35@gmail.com>
2019-01-23 18:51:40 +0300

    upd

--- platform.mk
+++ platform.mk
@@ -43,10 +43,8 @@ PRODUCT_PACKAGES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-	AntHalService \
-	antradio_app \
-	com.dsi.ant.antradio_library \
-	libantradio
+    AntHalService \
+    com.dsi.ant.antradio_library
 
 PRODUCT_COPY_FILES += \
 	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
